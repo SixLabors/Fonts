9a1ecd9e2f1ae2c170e1052405a0bd9d5fc3d29a
Scott Williams <tocsoft@gmail.com>
2017-04-12 07:37:06 +0100

    add test coverage for accented glyphs

---
+++ tests/SixLabors.Fonts.Tests/Accents.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SixLabors.Fonts.Tests
+{
+    public class Accents
+    {
+        [Theory]
+        [InlineData('á')]
+        [InlineData('é')]
+        [InlineData('í')]
+        [InlineData('ó')]
+        [InlineData('ú')]
+        [InlineData('ç')]
+        [InlineData('ã')]
+        [InlineData('õ')]
+        public void MeasuringAccentedCharacterDoesNotThrow(char c)
+        {
+            var arial = FontCollection.SystemFonts.Find("Arial");
+            var font = new Font(arial, 1f, FontStyle.Regular);
+
+            var size = new TextMeasurer().MeasureText(c.ToString(), font, 72);
+        }
+
+        [Theory]
+        [InlineData('á')]
+        [InlineData('é')]
+        [InlineData('í')]
+        [InlineData('ó')]
+        [InlineData('ú')]
+        [InlineData('ç')]
+        [InlineData('ã')]
+        [InlineData('õ')]
+        public void MeasuringWordWithAccentedCharacterDoesNotThrow(char c)
+        {
+            var arial = FontCollection.SystemFonts.Find("Arial");
+            var font = new Font(arial, 1f, FontStyle.Regular);
+
+            var size = new TextMeasurer().MeasureText($"abc{c}def", font, 72);
+        }
+    }
+}
