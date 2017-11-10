8e4587eba2332a485b5d61435c79040e31117c66
ngjunyen@gmail.com <ngjunyen@gmail.com>
2017-11-10 23:33:11 +1100

    change test back to expect correct value

--- tests/SixLabors.Fonts.Tests/TextLayoutTests.cs
+++ tests/SixLabors.Fonts.Tests/TextLayoutTests.cs
@@ -114,8 +114,8 @@ namespace SixLabors.Fonts.Tests
 
         [Theory]
         [InlineData("hello world", 10, 310)]
-        [InlineData("hello world hello world",
-            40, // 30 actual line height + 10 actual height
+        [InlineData("hello world hello world hello world",
+            70, // 30 actual line height * 2 + 10 actual height
             310)]
         public void MeasureTextWordWrapping(string text, float height, float width)
         {
