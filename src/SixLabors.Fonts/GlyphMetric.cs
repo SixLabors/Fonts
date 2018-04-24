ac689dbcf5b294d785c4de793a0d353912701366
woutware <35376607+woutware@users.noreply.github.com>
2018-04-24 23:24:16 +0200

    Extended TextMeasurer to be able to measure character bounds.

---
+++ src/SixLabors.Fonts/GlyphMetric.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.Primitives;
+
+namespace SixLabors.Fonts
+{
+    /// <summary>
+    /// Represents a <see cref="Glyph"/> metric.
+    /// </summary>
+    public struct GlyphMetric
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GlyphMetric"/> struct.
+        /// </summary>
+        /// <param name="character">The character.</param>
+        /// <param name="bounds">The bounds.</param>
+        /// <param name="isControlCharacter">Whether the character is a control character.</param>
+        public GlyphMetric(char character, RectangleF bounds, bool isControlCharacter) 
+        {
+            this.Character = character;
+            this.Bounds = bounds;
+            this.IsControlCharacter = isControlCharacter;
+        }
+
+        /// <summary>
+        /// Gets the character.
+        /// </summary>
+        public char Character { get; }
+
+        /// <summary>
+        /// Gets the character bounds.
+        /// </summary>
+        public RectangleF Bounds { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether the character is a control character.
+        /// </summary>
+        public bool IsControlCharacter { get; }
+
+        /// <inheritdoc/>
+        public override string ToString() {
+            return $"Char: {this.Character}, bounds: {this.Bounds}, is control char: {this.IsControlCharacter}";
+        }
+    }
+}
