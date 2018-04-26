f24e96f32e15ea5638a82f7d2ad47c1ff3799117
woutware <35376607+woutware@users.noreply.github.com>
2018-04-26 13:42:16 +0200

    Added unit test for testing accessing vertical font metrics properties.

--- tests/SixLabors.Fonts.Tests/FakeFont.cs
+++ tests/SixLabors.Fonts.Tests/FakeFont.cs
@@ -1,4 +1,24 @@
-﻿namespace SixLabors.Fonts.Tests
-{
+﻿using Xunit;
 
+using SixLabors.Fonts.Tests.Fakes;
+
+namespace SixLabors.Fonts.Tests
+{
+    public class FakeFont {
+        [Fact]
+        public void TestFontMetricProperties() {
+            Font fakeFont = CreateFont("A");
+            Assert.Equal(30, fakeFont.EmSize);
+            Assert.Equal(35, fakeFont.Ascender);
+            Assert.Equal(8, fakeFont.Descender);
+            Assert.Equal(12, fakeFont.LineGap);
+            Assert.Equal(35 - 8 + 12, fakeFont.LineHeight);
+        }
+
+        public static Font CreateFont(string text) {
+            FontCollection fc = new FontCollection();
+            Font d = fc.Install(FakeFontInstance.CreateFontWithVaryingVerticalFontMetrics(text)).CreateFont(12);
+            return new Font(d, 1);
+        }
+    }
 }
