eccc0587bf0ebd8746ed17860214a1a56b2e6dbf
James Jackson-South <james_south@hotmail.com>
2021-07-19 15:54:59 +1000

    Add better GlyphMetric constructor docs.

--- src/SixLabors.Fonts/GlyphMetric.cs
+++ src/SixLabors.Fonts/GlyphMetric.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Six Labors.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
 using SixLabors.Fonts.Unicode;
 
 namespace SixLabors.Fonts
@@ -13,17 +14,21 @@ namespace SixLabors.Fonts
         /// <summary>
         /// Initializes a new instance of the <see cref="GlyphMetric"/> struct.
         /// </summary>
-        /// <param name="codePoint">Unicode codepoint for the glyph.</param>
-        /// <param name="bounds">The bounds.</param>
-        public GlyphMetric(char codePoint, FontRectangle bounds)
-            : this(new CodePoint(codePoint), bounds)
+        /// <param name="value">The char representing the UTF-16 code unit for the glyph.</param>
+        /// <param name="bounds">The glyph bounds.</param>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="value"/> represents a UTF-16 surrogate code point
+        /// U+D800..U+DFFF, inclusive.
+        /// </exception>
+        public GlyphMetric(char value, FontRectangle bounds)
+            : this(new CodePoint(value), bounds)
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GlyphMetric"/> struct.
         /// </summary>
-        /// <param name="codePoint">Unicode codepoint for the glyph.</param>
+        /// <param name="codePoint">The Unicode codepoint for the glyph.</param>
         /// <param name="bounds">The glyph bounds.</param>
         public GlyphMetric(CodePoint codePoint, FontRectangle bounds)
         {
