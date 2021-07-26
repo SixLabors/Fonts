78257c9f35ae348cec46ca45ba5dca3c29a728a5
James Jackson-South <james_south@hotmail.com>
2021-07-26 17:47:12 +1000

    Add vertical font metrics tests

--- tests/SixLabors.Fonts.Tests/FontMetricsTests.cs
+++ tests/SixLabors.Fonts.Tests/FontMetricsTests.cs
@@ -29,6 +29,27 @@ namespace SixLabors.Fonts.Tests
         }
 
         [Fact]
+        public void FontMetricsVerticalFontMatchesReference()
+        {
+            // Compared to EveryFonts TTFDump metrics
+            // https://everythingfonts.com/ttfdump
+            var collection = new FontCollection();
+            FontFamily family = collection.Install(TestFonts.NotoSansSCThinFile);
+            Font font = family.CreateFont(12);
+
+            Assert.Equal(1000, font.FontMetrics.UnitsPerEm);
+            Assert.Equal(806, font.FontMetrics.Ascender);
+            Assert.Equal(-256, font.FontMetrics.Descender);
+            Assert.Equal(90, font.FontMetrics.LineGap);
+            Assert.Equal(1152, font.FontMetrics.LineHeight);
+            Assert.Equal(1000, font.FontMetrics.AdvanceWidthMax);
+            Assert.Equal(1000, font.FontMetrics.AdvanceHeightMax);
+
+            Assert.False(font.IsBold);
+            Assert.False(font.IsItalic);
+        }
+
+        [Fact]
         public void GlyphMetricsMatchesReference()
         {
             // Compared to FontForge metrics
@@ -52,6 +73,34 @@ namespace SixLabors.Fonts.Tests
             Assert.Equal(0, glyphMetrics.LeftSideBearing);
             Assert.Equal(0, glyphMetrics.TopSideBearing);
             Assert.Equal(GlyphType.Standard, glyphMetrics.GlyphType);
+        }
+
+        [Fact]
+        public void GlyphMetricsVerticalMatchesReference()
+        {
+            // Compared to EveryFonts TTFDump metrics
+            // https://everythingfonts.com/ttfdump
+            var collection = new FontCollection();
+            FontFamily family = collection.Install(TestFonts.NotoSansSCThinFile);
+            Font font = family.CreateFont(12);
+
+            var codePoint = new CodePoint('A');
+            GlyphMetrics glyphMetrics = font.FontMetrics.GetGlyphMetrics(codePoint);
+            GlyphMetrics glyphMetrics1 = font.GetGlyph(codePoint).GlyphMetrics;
+
+            Assert.Equal(glyphMetrics, glyphMetrics1);
+
+            // Fallback glyph at position 0.
+            Assert.Equal(codePoint, glyphMetrics.Codepoint);
+            Assert.Equal(font.FontMetrics.UnitsPerEm, glyphMetrics.UnitsPerEm);
+            Assert.Equal(glyphMetrics.UnitsPerEm * 72F, glyphMetrics.ScaleFactor);
+            Assert.Equal(364, glyphMetrics.AdvanceWidth);
+            Assert.Equal(1000, glyphMetrics.AdvanceHeight);
+            Assert.Equal(265, glyphMetrics.Width);
+            Assert.Equal(666, glyphMetrics.Height);
+            Assert.Equal(33, glyphMetrics.LeftSideBearing);
+            Assert.Equal(134, glyphMetrics.TopSideBearing);
+            Assert.Equal(GlyphType.Fallback, glyphMetrics.GlyphType);
         }
     }
 }
