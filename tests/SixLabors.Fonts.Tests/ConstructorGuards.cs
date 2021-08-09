b1aa01c85320cb882a24cc68ccd722dc7357355d
James Jackson-South <james_south@hotmail.com>
2021-08-10 01:11:29 +1000

    Update ConstructorGuards.cs

--- tests/SixLabors.Fonts.Tests/ConstructorGuards.cs
+++ tests/SixLabors.Fonts.Tests/ConstructorGuards.cs
@@ -11,12 +11,32 @@ namespace SixLabors.Fonts.Tests
         [Fact]
         public void FontClass_DefaultFontFamilyThrowsException()
         {
-            ArgumentException ex = Assert.Throws<ArgumentException>(() => new Font(default(FontFamily), 0f, FontStyle.Regular));
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                () => new Font(default(FontFamily), FontStyle.Regular));
+
+            Assert.Equal("family", ex.ParamName);
+        }
+
+        [Fact]
+        public void FontClass_DefaultFontFamilyWithSizeThrowsException()
+        {
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                () => new Font(default(FontFamily), 0f, FontStyle.Regular));
+
             Assert.Equal("family", ex.ParamName);
         }
 
         [Fact]
         public void FontClass_NullFontThrowsException()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(
+                () => new Font(null, FontStyle.Regular));
+
+            Assert.Equal("prototype", ex.ParamName);
+        }
+
+        [Fact]
+        public void FontClass_NullWithSizeFontThrowsException()
         {
             ArgumentNullException ex = Assert.Throws<ArgumentNullException>(
                 () => new Font(null, 0f, FontStyle.Regular));
