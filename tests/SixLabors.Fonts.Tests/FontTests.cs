779827b44b9db2f9c995d785a5b576d36db483c3
James Jackson-South <james_south@hotmail.com>
2021-08-10 22:53:31 +1000

    More fonts tests

--- tests/SixLabors.Fonts.Tests/FontTests.cs
+++ tests/SixLabors.Fonts.Tests/FontTests.cs
@@ -2,11 +2,12 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+using System.IO;
 using Xunit;
 
 namespace SixLabors.Fonts.Tests
 {
-    public class ConstructorGuards
+    public class FontTests
     {
         [Fact]
         public void FontClass_DefaultFontFamilyThrowsException()
@@ -42,6 +43,39 @@ namespace SixLabors.Fonts.Tests
                 () => new Font(null, 0f, FontStyle.Regular));
 
             Assert.Equal("prototype", ex.ParamName);
+        }
+
+        [Fact]
+        public void FontClassWithPath_SetProperties()
+        {
+            var collection = new FontCollection();
+            FontFamily family = collection.Add(TestFonts.CarterOneFile);
+            var font = new Font(family, 12);
+
+            Assert.Equal("Carter One", font.Name);
+            Assert.Equal(12, font.Size);
+            Assert.Equal(FontStyle.Regular, font.RequestedStyle);
+            Assert.False(font.IsBold);
+            Assert.False(font.IsItalic);
+            Assert.True(font.TryGetPath(out string path));
+            Assert.NotNull(path);
+        }
+
+        [Fact]
+        public void FontClassNoPath_SetProperties()
+        {
+            var collection = new FontCollection();
+            using Stream stream = TestFonts.CarterOneFileData();
+            FontFamily family = collection.Add(stream);
+            var font = new Font(family, 12);
+
+            Assert.Equal("Carter One", font.Name);
+            Assert.Equal(12, font.Size);
+            Assert.Equal(FontStyle.Regular, font.RequestedStyle);
+            Assert.False(font.IsBold);
+            Assert.False(font.IsItalic);
+            Assert.False(font.TryGetPath(out string path));
+            Assert.Null(path);
         }
     }
 }
