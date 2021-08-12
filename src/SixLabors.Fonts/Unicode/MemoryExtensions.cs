bc371bb5cce147ecf8fb32e74f01052ef193bd1d
James Jackson-South <james_south@hotmail.com>
2021-08-13 00:52:23 +1000

    Expose Grapheme enumeration

--- src/SixLabors.Fonts/Unicode/MemoryExtensions.cs
+++ src/SixLabors.Fonts/Unicode/MemoryExtensions.cs
@@ -31,5 +31,27 @@ namespace SixLabors.Fonts.Unicode
         /// <returns>The <see cref="SpanCodePointEnumerator"/>.</returns>
         public static SpanCodePointEnumerator EnumerateCodePoints(this Span<char> span)
             => new SpanCodePointEnumerator(span);
+
+        /// <summary>
+        /// Returns an enumeration of Grapheme instances from the provided span.
+        /// </summary>
+        /// <param name="span">The readonly span of char elements representing the text to enumerate.</param>
+        /// <remarks>
+        /// Invalid sequences will be represented in the enumeration by <see cref="GraphemeClusterClass.Any"/>.
+        /// </remarks>
+        /// <returns>The <see cref="SpanGraphemeEnumerator"/>.</returns>
+        public static SpanGraphemeEnumerator EnumerateGraphemes(this ReadOnlySpan<char> span)
+            => new SpanGraphemeEnumerator(span);
+
+        /// <summary>
+        /// Returns an enumeration of Grapheme instances from the provided span.
+        /// </summary>
+        /// <param name="span">The span of char elements representing the text to enumerate.</param>
+        /// <remarks>
+        /// Invalid sequences will be represented in the enumeration by <see cref="GraphemeClusterClass.Any"/>.
+        /// </remarks>
+        /// <returns>The <see cref="SpanGraphemeEnumerator"/>.</returns>
+        public static SpanGraphemeEnumerator EnumerateGraphemes(this Span<char> span)
+            => new SpanGraphemeEnumerator(span);
     }
 }
