e05aae789d38079968e7dd4c3a68ba2985bef2e4
Brian Popow <popow@gmx.de>
2021-09-23 17:59:22 +0200

    Move Class1Record to a separate file

--- src/SixLabors.Fonts/Tables/AdvancedTypographic/GPos/LookupType2SubTable.cs
+++ src/SixLabors.Fonts/Tables/AdvancedTypographic/GPos/LookupType2SubTable.cs
@@ -289,30 +289,6 @@ namespace SixLabors.Fonts.Tables.AdvancedTypographic.GPos
 
                 return false;
             }
-
-            internal sealed class Class1Record
-            {
-                private Class1Record(Class2Record[] class2Records) => this.Class2Records = class2Records;
-
-                public Class2Record[] Class2Records { get; }
-
-                public static Class1Record Load(BigEndianBinaryReader reader, int class2Count, ValueFormat valueFormat1, ValueFormat valueFormat2)
-                {
-                    // +--------------+----------------------------+---------------------------------------------+
-                    // | Type         | Name                       | Description                                 |
-                    // +==============+============================+=============================================+
-                    // | Class2Record | class2Records[class2Count] | Array of Class2 records, ordered by classes |
-                    // |              |                            | in classDef2.                               |
-                    // +--------------+----------------------------+---------------------------------------------+
-                    var class2Records = new Class2Record[class2Count];
-                    for (int i = 0; i < class2Records.Length; i++)
-                    {
-                        class2Records[i] = new Class2Record(reader, valueFormat1, valueFormat2);
-                    }
-
-                    return new Class1Record(class2Records);
-                }
-            }
         }
     }
 }
