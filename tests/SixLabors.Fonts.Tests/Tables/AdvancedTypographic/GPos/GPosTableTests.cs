ed665e19a2e1cc268f55369f08cc44f080ede748
Brian Popow <popow@gmx.de>
2021-09-23 17:50:29 +0200

    Add test for SingleAdjustmentPositioning format 2

--- tests/SixLabors.Fonts.Tests/Tables/AdvancedTypographic/GPos/GPosTableTests.cs
+++ tests/SixLabors.Fonts.Tests/Tables/AdvancedTypographic/GPos/GPosTableTests.cs
@@ -13,12 +13,46 @@ namespace SixLabors.Fonts.Tests.Tables.AdvancedTypographic.GPos
             // arrange
             Font gPosFont = new FontCollection().Add(TestFonts.GposLookupType1Format1).CreateFont(8);
             var renderer = new ColorGlyphRenderer();
-            string testStr = "\u0015\u0014"; // second character the XPlacement should be adjusted by minus 200
+            string testStr = "\u0015\u0014"; // second character XPlacement should be adjusted by minus 200
             int[] expectedGlyphIndices = { 23, 22 };
             FontRectangle[] expectedFontRectangles =
             {
-                new FontRectangle(2.0608f, 12.8703995f, 6.2592f, 4.5312f),
-                new FontRectangle(9.10080051f, 12.8639984f, 6.2592f, 4.5056f),
+                new(2.0608f, 12.8703995f, 6.2592f, 4.5312f),
+                new(9.10080051f, 12.8639984f, 6.2592f, 4.5056f),
+            };
+
+            // act
+            TextRenderer.RenderTextTo(renderer, testStr, new RendererOptions(gPosFont)
+            {
+                ApplyKerning = true
+            });
+
+            // assert
+            Assert.Equal(expectedGlyphIndices.Length, renderer.GlyphKeys.Count);
+            Assert.Equal(expectedFontRectangles.Length, renderer.GlyphRects.Count);
+            for (int i = 0; i < expectedGlyphIndices.Length; i++)
+            {
+                Assert.Equal(expectedGlyphIndices[i], renderer.GlyphKeys[i].GlyphIndex);
+            }
+
+            for (int i = 0; i < expectedFontRectangles.Length; i++)
+            {
+                Assert.Equal(expectedFontRectangles[i], renderer.GlyphRects[i]);
+            }
+        }
+
+        [Fact]
+        public void SingleAdjustmentPositioning_Format2_Works()
+        {
+            // arrange
+            Font gPosFont = new FontCollection().Add(TestFonts.GposLookupType1Format2).CreateFont(8);
+            var renderer = new ColorGlyphRenderer();
+            string testStr = "\u0015\u0014"; // second character XPlacement should be adjusted by minus 200
+            int[] expectedGlyphIndices = { 23, 22 };
+            FontRectangle[] expectedFontRectangles =
+            {
+                new(2.0608f, 12.8703995f, 6.2592f, 4.5312f),
+                new(9.740801f, 12.8639984f, 6.2592f, 4.5056f),
             };
 
             // act
