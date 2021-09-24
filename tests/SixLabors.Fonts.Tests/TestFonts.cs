48158279fc34db3ef96dc3edef41a7fb9bbd33b0
Brian Popow <popow@gmx.de>
2021-09-24 13:30:15 +0200

    Add test for LookupType7SubTable, Format 3

--- tests/SixLabors.Fonts.Tests/TestFonts.cs
+++ tests/SixLabors.Fonts.Tests/TestFonts.cs
@@ -134,6 +134,12 @@ namespace SixLabors.Fonts.Tests
         /// </summary>
         public static string GposLookupType7Format2 => GetFullPath("GposLookupType7Format2.ttf");
 
+        /// <summary>
+        /// Gets a gpos test font file which has the following substitution for unit tests:
+        /// - Contextual Positioning Subtables, Format 3: "\u0014\u0015\u0016" XPlacement plus 20.
+        /// </summary>
+        public static string GposLookupType7Format3 => GetFullPath("GposLookupType7Format3.ttf");
+
         public static string SimpleTrueTypeCollection => GetFullPath("Sample.ttc");
 
         public static string WhitneyBookFile => GetFullPath("whitney-book.ttf");
