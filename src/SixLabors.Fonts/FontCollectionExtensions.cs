a60909415d2b26cb951645cdbc5b684edc4de34b
LeaFrock <petergeorge@126.com>
2022-02-18 09:52:21 +0800

    Add an overload of `AddSystemFonts`

--- src/SixLabors.Fonts/FontCollectionExtensions.cs
+++ src/SixLabors.Fonts/FontCollectionExtensions.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Six Labors.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
+
 namespace SixLabors.Fonts
 {
     /// <summary>
@@ -17,9 +19,28 @@ namespace SixLabors.Fonts
         {
             // This cast is safe because our underlying SystemFontCollection implements
             // both interfaces separately.
-            foreach (FontMetrics? metric in (IReadOnlyFontMetricsCollection)SystemFonts.Collection)
+            foreach (FontMetrics metric in (IReadOnlyFontMetricsCollection)SystemFonts.Collection)
             {
                 ((IFontMetricsCollection)collection).AddMetrics(metric);
+            }
+
+            return collection;
+        }
+
+        /// <summary>
+        /// Adds the fonts from the <see cref="SystemFonts"/> collection to this <see cref="FontCollection"/>.
+        /// </summary>
+        /// <param name="collection">The font collection.</param>
+        /// <param name="match">The System.Predicate delegate that defines the conditions of <see cref="FontMetrics"/> to add into the font collection.</param>
+        /// <returns>The <see cref="FontCollection"/> containing the system fonts.</returns>
+        public static FontCollection AddSystemFonts(this FontCollection collection, Predicate<FontMetrics> match)
+        {
+            foreach (FontMetrics metric in (IReadOnlyFontMetricsCollection)SystemFonts.Collection)
+            {
+                if (match(metric))
+                {
+                    ((IFontMetricsCollection)collection).AddMetrics(metric);
+                }
             }
 
             return collection;
