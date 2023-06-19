648a65ab0efa58d582ab8cf4bbaf3275706708dc
James Jackson-South <james_south@hotmail.com>
2023-06-19 21:59:15 +1000

    Update Issues_269.cs

--- tests/SixLabors.Fonts.Tests/Issues/Issues_269.cs
+++ tests/SixLabors.Fonts.Tests/Issues/Issues_269.cs
@@ -17,21 +17,7 @@ namespace SixLabors.Fonts.Tests.Issues
             Font font = new FontCollection().Add(TestFonts.AliceFrancesHMKRegularFile).CreateFont(25);
 
             FontRectangle size = TextMeasurer.MeasureSize("H", new TextOptions(font));
-
-            // TODO: We should probably drop the 32bit test runner.
-#if NET472
-            if (Environment.Is64BitProcess)
-            {
-                Assert.Equal(32, size.Width, 1);
-            }
-            else
-            {
-                Assert.Equal(33, size.Width, 1);
-            }
-#else
             Assert.Equal(32, size.Width, 1);
-#endif
-
             Assert.Equal(27, size.Height, 1);
         }
     }
